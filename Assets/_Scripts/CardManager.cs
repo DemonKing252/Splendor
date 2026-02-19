e23a74845dedfbaa5b9fbf2e78d0c032a6d72616
DemonKing252 <46793138+DemonKing252@users.noreply.github.com>
2026-02-19 01:29:08 -0500

    Card generator

--- Assets/_Scripts/CardManager.cs
+++ Assets/_Scripts/CardManager.cs
@@ -1,8 +1,149 @@
 using System;
+using System.Collections.Generic;
+using System.Data;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CardManager : MonoBehaviour
 {
+
+    public int[] RandomIndexes(int count, int maxInclusive)
+    {
+        List<int> values = new List<int>();
+
+        int[] indexes = new int[count];
+        indexes[0] = UnityEngine.Random.Range(0, maxInclusive+1);
+        values.Add(indexes[0]);
+
+        for(int i = 1; i < count; i++)
+        {
+            do
+            {
+                indexes[i] = UnityEngine.Random.Range(0, maxInclusive+1);
+            } while (values.Contains(indexes[i]));
+            values.Add(indexes[i]);
+        }
+
+        return indexes;
+    }
+
+    public string IntArrayToList(int[] arr)
+    {
+        string str = null;
+        str += "{";
+        foreach(int value in arr)
+        {
+            str += value + ", ";
+        }
+        str += "}";
+        return str;
+    }
+
+    public void ScrambleBoard(int cardCount)
+    {
+        /*
+            ** All Inclusive Numbers **
+
+            Prestige = 40% for 0, 30% for 1, 30% for 2
+            Within Prest 0: Another random number between 1 and 2:
+            If 1 -> one gem type costing 2-3
+            If 2 -> two gem types costing 1-2
+
+            Within Prestige 1: Another random number between 1 and 3:
+            If 1 -> one gem type costing 3-4 
+            If 2 -> two gem types costing 2-3
+            If 3 -> three gem types costing 1-2
+
+            Within Prestige 2: Another random number between 1 and 3:
+            If 1 -> one gem type costing 6-7 
+            If 2 -> two gem types costing 4-5
+            If 3 -> two gem types costing 3-4 
+            If 4 -> four gem types costing 2-3
+        */
+
+        int prestige = 0;
+        
+        /*
+            diamondCount,
+            rubyCount,
+            saphireCount,
+            onyxCount,
+            emeraldCount
+        */
+
+        int[] gemCosts = new int[5]
+        {
+            0, 0, 0, 0, 0
+        };
+        
+        Action<int, int, int> PopulateIndexArray = (count, minInclusive, maxInclusive) =>
+        {
+            int[] randIndexes = RandomIndexes(count, 4);
+            
+            for(int idx = 0; idx < count; idx++)
+                gemCosts[randIndexes[idx]] = UnityEngine.Random.Range(minInclusive, maxInclusive + 1);
+        };
+
+        for (int i = 0; i < cardCount; i++)
+        {
+            
+            float prestiegeRand = UnityEngine.Random.Range(0f, 100f);
+
+            if (prestiegeRand <= 40f)   // 0 Prestige Point.
+            {
+                prestige = 0;
+                int gemPrefabCount = UnityEngine.Random.Range(1, 3); // 1 or 2
+
+                switch(gemPrefabCount)
+                {
+                    case 1: PopulateIndexArray(1, 2, 3); break;
+                    case 2: PopulateIndexArray(2, 1, 2); break;
+                }
+            }
+            else if (prestiegeRand <= 70f) // 1 Prestige Point.
+            {
+                prestige = 1;
+                int gemPrefabCount = UnityEngine.Random.Range(1, 4); // 1/2/3
+            
+                switch(gemPrefabCount)
+                {
+                    case 1: PopulateIndexArray(1, 3, 4); break;
+                    case 2: PopulateIndexArray(2, 2, 3); break;
+                    case 3: PopulateIndexArray(3, 1, 2); break;
+                }
+            }
+            else // 2 Prestige Points.
+            {
+                prestige = 2;
+                int gemPrefabCount = UnityEngine.Random.Range(1, 5); // 1/2/3/4
+
+                switch(gemPrefabCount)
+                {
+                    case 1: PopulateIndexArray(1, 6, 7); break;
+                    case 2: PopulateIndexArray(2, 4, 5); break;
+                    case 3: PopulateIndexArray(3, 3, 4); break;
+                    case 4: PopulateIndexArray(4, 2, 3); break;
+                }
+            }
+
+            Debug.Log(i + " - Gem Stone Values: " + IntArrayToList(gemCosts));
+            
+            GemStoneType randGemType = (GemStoneType)UnityEngine.Random.Range(0, (int)GemStoneType.Count); 
+
+            cards[i].SetCard(randGemType, prestige, gemCosts[0], gemCosts[1], gemCosts[2], gemCosts[3], gemCosts[4]);
+            
+            for(int idx = 0; idx < gemCosts.Length; idx++)
+                gemCosts[idx] = 0;
+        }
+    }
+
+    // TODO: RescrambleBoard at Index I
+    public void RescrambleBoard()
+    {
+        
+    }
+
+
     [SerializeField]
     private Transform cardBoardTransform;
 
@@ -24,77 +165,7 @@ public class CardManager : MonoBehaviour
             cards[i] = cardBoardTransform.GetChild(i).GetComponent<CardBehaviour>();
         }
 
-        //TODO: Improve randomness:
-        /*
-            ** All Inclusive Numbers **
-
-            Prestige = 40% for 0, 30% for 1, 30% for 2
-            Within Prest 0: Another random number between 1 and 2:
-            If 1 -> one gem type costing 2-3
-            If 2 -> two gem types costing 1-2
-
-            Within Prestige 1: Another random number between 1 and 3:
-            If 1 -> one gem type costing 3-4 
-            If 2 -> two gem types costing 2-3
-            If 3 -> three gem types costing 1-2
-
-            Within Prestige 2: Another random number between 1 and 3:
-            If 1 -> one gem type costing 6-7 
-            If 2 -> two gem types costing 4-5
-            If 3 -> two gem types costing 3-4 
-            If 4 -> four gem types costing 2-3
-        */
-        
-        for (int i = 0; i < cardCount; i++)
-        {
-            int randomPrest = UnityEngine.Random.Range(0, 3);
-
-            int randDiamond;
-            int randRuby;
-            int randSaphire;
-            int randOnyx;
-            int randEmerald;
-
-            if (randomPrest == 0)
-            {
-                int min_range = 0;
-                int max_range = 2;
-
-                randDiamond = UnityEngine.Random.Range(min_range, max_range);
-                randRuby = UnityEngine.Random.Range(min_range, max_range);
-                randSaphire = UnityEngine.Random.Range(min_range, max_range);
-                randOnyx = UnityEngine.Random.Range(min_range, max_range);
-                randEmerald = UnityEngine.Random.Range(min_range, max_range);    
-            }
-            else if (randomPrest == 1)
-            {                
-                int min_range = 0;
-                int max_range = 3;
-
-                randDiamond = UnityEngine.Random.Range(min_range, max_range);
-                randRuby = UnityEngine.Random.Range(min_range, max_range);
-                randSaphire = UnityEngine.Random.Range(min_range, max_range);
-                randOnyx = UnityEngine.Random.Range(min_range, max_range);
-                randEmerald = UnityEngine.Random.Range(min_range, max_range);    
-            }
-            else
-            {                
-                int min_range = 0;
-                int max_range = 4;
-
-                randDiamond = UnityEngine.Random.Range(min_range, max_range);
-                randRuby = UnityEngine.Random.Range(min_range, max_range);
-                randSaphire = UnityEngine.Random.Range(min_range, max_range);
-                randOnyx = UnityEngine.Random.Range(min_range, max_range);
-                randEmerald = UnityEngine.Random.Range(min_range, max_range);    
-            }
-
-            GemStoneType randGemType = (GemStoneType)UnityEngine.Random.Range(0, (int)GemStoneType.Count); 
-
-            cards[i].SetCard(randGemType, randomPrest, randDiamond, randRuby, randSaphire, randOnyx, randEmerald);
-        }
-        
-        
+        ScrambleBoard(cardCount);
     }
     // --- Helper Functions ---
 
